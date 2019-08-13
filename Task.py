ac6d981d57384784748edbc5ed1622e249e067a3
Selevn <van000200136@gmail.com>
2019-08-13 19:50:09 +0300

    Task manager backend

---
+++ Task.py
@@ -0,0 +1,10 @@
+#Task Manager backend
+#coding: utf-8
+
+class Task:
+    def __init__(self, header, body, status):
+        self.header, self.body, self.status = header, body, status
+    
+    def add(self):
+        pass
+    #add it into 
