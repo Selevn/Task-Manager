106a8ec94a675f54eb7c4c9e6dc686ae889a65e7
Selevn <van000200136@gmail.com>
2019-08-20 13:58:06 +0300

    alpha with basic functional

--- Task.py
+++ Task.py
@@ -14,19 +14,27 @@ class Task:
 class DataBase:
     
     
-    def __init__(self, name = 'mydb.db'):
-        
+    def __init__(self, table = 'tasks', name = 'mydb.db'):
+        self.name = name
+        self.table = table
         self.conn = sqlite3.connect(name)
         self.cursor = self.conn.cursor()
         
-    def createtable(self, name = 'tasks'):
+    def change_table(self, new_name):
+        self.table = new_name
         try:
-            self.cursor.execute('''CREATE TABLE {0} (id int primary key, header varchar, body text, status varchar)'''.format(name))
+            self.cursor.execute('''CREATE TABLE {0} (id int primary key, header varchar, body text, status varchar)'''.format(self.table))
+        except sqlite3.OperationalError:
+            pass        
+        
+    def createtable(self):
+        try:
+            self.cursor.execute('''CREATE TABLE {0} (id int primary key, header varchar, body text, status varchar)'''.format(self.table))
         except sqlite3.OperationalError:
             pass
     
-    def add(self, header, body, status, table = 'tasks',):
-        self.cursor.execute('''SELECT id FROM {0} ORDER BY id DESC'''.format(table))
+    def add(self, header, body, status):
+        self.cursor.execute('''SELECT id FROM {0} ORDER BY id DESC'''.format(self.table))
         #print('q')
         #print(self.cursor.fetchone())
         try:
@@ -34,26 +42,36 @@ class DataBase:
         except TypeError:
             quit = -1
             
-        self.cursor.execute('''INSERT INTO {0} (id, header, body, status) VALUES (?, ?, ?, ?)'''.format(table), (quit+1, header, body, status))
+        self.cursor.execute('''INSERT INTO {0} (id, header, body, status) VALUES (?, ?, ?, ?)'''.format(self.table), (quit+1, header, body, status))
         self.conn.commit()
         
-    def switch_status(self, id_, end_status, table = 'tasks'):
+    def switch_status(self, id_, end_status):
         self.status = end_status
-        sqlrequest = '''UPDATE {0} SET status = '{1}' WHERE id = {2}'''.format(table, end_status, id_)
+        sqlrequest = '''UPDATE {0} SET status = '{1}' WHERE id = {2}'''.format(self.table, end_status, id_)
         #print(sqlrequest)
         self.cursor.execute(sqlrequest)
         self.conn.commit()
     
-    def get(self, id_, table = 'tasks'):
-        strs = '''SELECT * FROM {0} WHERE id = {1} '''.format(table, id_)
+    def get(self, id_):
+        strs = '''SELECT * FROM {0} WHERE id = {1} '''.format(self.table, id_)
         listt = self.cursor.execute(strs)
         for i in listt:
             quit = i
         return quit
     
-    def get_all(self, table = 'tasks'):
-        strs = '''SELECT * FROM {0} ORDER BY id'''.format(table)
+    def get_all(self):
+        strs = '''SELECT * FROM {0} ORDER BY id'''.format(self.table)
         return self.cursor.execute(strs)
+    
+    def get_daily(self):
+        strs = '''SELECT * FROM {0} WHERE status = "daily" ORDER BY id'''.format(self.table)
+        return self.cursor.execute(strs)    
+    
+    def send(self, string):
+        self.cursor.execute(string)
+        self.conn.commit()
+    
+        #UPDATE tablename SET pole1="message" WHERE id="1" and id="15";    
         
     
         
@@ -62,9 +80,9 @@ class DataBase:
         self.conn.close()
 
 
-q = DataBase('olddb.db')
-q.createtable()
+#q = DataBase('olddb.db')
+#q.createtable()
 #q.add(1, 'of', 'aq')
 #q.add(2, 'of', 'aq')
-q.switch_status(2,'switched')
-q.dbclose()
+#q.switch_status(2,'switched')
+#q.dbclose()
